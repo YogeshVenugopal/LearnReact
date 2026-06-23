80b7b6976852ca3b7a124c9f617ed1aade91916c
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-06-23 09:45:53 +0530

    Feat : Completed the ui card  design

---
+++ Day - 1(JSX Component)/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from './Card'
+const App = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+        <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ Day - 1(JSX Component)/src/Card.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import person from '/Images/person.png'
+import {Heart, ThumbsUp, Plus} from 'lucide-react';
+
+const Card = () => {
+  return (
+    <div className='w-[25rem] h-[30rem] rounded-[18px] shadow-lg inset-shadow-sm flex items-center justify-center text-white'>
+        <div className='w-[calc(100%-12px)] h-[calc(100%-12px)] rounded-[14px] bg-green-700 relative overflow-hidden'>
+            <img src={person} alt="Profile Images" className='w-full h-full object-cover z-0'/>
+            <div className='absolute w-full h-1/3 bottom-0 right-0 bg-green-800/50 blur-xl z-10' />
+            <div className='absolute w-full h-1/3 bottom-0 right-0 z-50'>
+                <div className='w-[90%] h-[90%] mx-auto z-50'>
+                    <h1 className='text-white text-2xl tex'>Name of the user</h1>
+                    <p className=' text-white/70 text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate distinctio eveniet praesentium, eum ex natus laborum placeat aut assumenda omnis.</p>
+                    <div className='w-full flex justify-around mt-2'>
+                        <div className='flex gap-1 items-center'>
+                            <Heart size={20}/> <p>2.3k</p>
+                        </div>
+                        <div className='flex gap-1 items-center'>
+                            <ThumbsUp  size={20}/> <p>1k</p>
+                        </div>
+                        <button className='px-5 py-2 rounded-3xl flex items-center gap-1 bg-white text-black'>
+                            <Plus /> Follow
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
