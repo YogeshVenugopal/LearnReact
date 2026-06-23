1cc6df814ec9dbee9bd2572d66f8fce6ea2c4e41
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-06-23 18:48:35 +0530

    UI : Insert image in the card

--- Day - 3(useState)/src/App.jsx
+++ Day - 3(useState)/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card from './Card'
+import person from '/Images/person.png'
+
 const App = () => {
   return (
     <div className='w-full h-screen flex flex-col items-center justify-center gap-3'>
       <h1 className='font-bold text-3xl'>Click the like button </h1>
-      <Card />
+      <Card profile={person}/>
     </div>
   )
 }
