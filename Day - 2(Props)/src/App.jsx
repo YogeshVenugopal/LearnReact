b77bf1108ef26d1b407388554aec6d1f63aa6269
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-06-23 10:28:56 +0530

    Feat !: implement the modern props method

--- Day - 2(Props)/src/App.jsx
+++ Day - 2(Props)/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Card from './Card'
+import person from '/Images/person.png'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='w-full h-screen flex items-center justify-center'>
+        <Card profile={person} name="Sample" desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente officia saepe vel perspiciatis? Qui ipsa tempore reiciendis distinctio molestias debitis!"/>
+        <Card />
     </div>
   )
 }
