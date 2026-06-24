8ad1a8b92a143a566ff9affa02037f46513943be
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-06-24 10:39:02 +0530

    Feat: set the mock data and create a base card component

---
+++ Day - 6(List and key)/src/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = (
+    id = 0,
+    name = "product name",
+    price = 0.00,
+    description = "Sample description for the given product"
+) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Card
---
+++ Day - 6(List and key)/src/data.js
@@ -0,0 +1,40 @@
+const val = {
+  "1": {
+    "id": 1,
+    "name": "Product 1",
+    "price": 10.99,
+    "description": "This is the description for Product 1."
+  },
+  "2": {
+    "id": 2,
+    "name": "Product 2",
+    "price": 19.99,
+    "description": "This is the description for Product 2."
+  },
+  "3": {
+    "id": 3,
+    "name": "Product 3",
+    "price": 5.99,
+    "description": "This is the description for Product 3."
+  },
+  "4": {
+    "id": 4,
+    "name": "Product 4",
+    "price": 15.99,
+    "description": "This is the description for Product 4."
+  },
+  "5": {
+    "id": 5,
+    "name": "Product 5",
+    "price": 12.99,
+    "description": "This is the description for Product 5."
+  },
+  "6": {
+    "id": 6,
+    "name": "Product 6",
+    "price": 8.99,
+    "description": "This is the description for Product 6."
+  }
+}
+
+export default val;
