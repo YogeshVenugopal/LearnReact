b13a0bf2c960df7c7cd574bced97a7ec8157182f
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-06-29 10:53:03 +0530

    Fix: All are revoked

--- Mock Prep/src/App.jsx
+++ Mock Prep/src/App.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LandingPage from './Pages/LandingPage'
-import Order from './Pages/Order'
-import Cart from './Pages/Cart'
-import Product from './Pages/Product'
 
 const App = () => {
   return (
-    
-      <Router>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/product' element={<Product />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/order' element={<Order />} />
-        </Routes>
-      </Router>
+    <div>
+      
+    </div>
   )
 }
 
--- Mock Prep/src/main.jsx
+++ Mock Prep/src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ProductProvider } from './Context/ProductContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-    <ProductProvider>
       <App />
-    </ProductProvider>
-  </StrictMode>,
+  </StrictMode>
 )
